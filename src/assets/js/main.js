77b2e4a8fdc0de084074ff1c31543bb3b3514cdb
Damir S <damir23.02@mail.ru>
2022-05-13 01:12:45 +0300

    search bar

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+    // SHOW SEARCH INPUT
+    $('#searchBtn').on('click', function(e) {
+        e.preventDefault();
+        $('#searchInput').addClass('active');
+    });
+
+    // $('#searchBtn').on('click', function(e) {
+    //     if (!$('#searchInput').val().length) {
+    //     e.preventDefault();
+    //     $('#searchInput').removeClass('active');
+    //     }
+    // });
+
+    // $('html').click(function(e) {
+    //     if (!$(e.target).closest('.header-search').length) {
+    //         $('#searchInput').removeClass('active');
+    //     }
+    // });
+
 });
